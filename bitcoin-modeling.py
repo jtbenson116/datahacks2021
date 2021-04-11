989567201fcbc9cdc0aecbe4f414a8d63bf0be26
tybens <tb19@princeton.edu>
2021-04-11 12:54:20 -0700

    Adding python scripts to run all of our work

---
+++ bitcoin-modeling.py
@@ -0,0 +1,171 @@
+import warnings
+warnings.filterwarnings('ignore')
+
+import pandas as pd
+import numpy as np
+import xgboost as xgb
+from sklearn.model_selection import train_test_split
+from sklearn.multiclass import OneVsRestClassifier, OneVsOneClassifier
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.linear_model import LogisticRegression, RidgeClassifier, SGDClassifier
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
+from sklearn.svm import LinearSVC
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import confusion_matrix, plot_confusion_matrix, accuracy_score, roc_auc_score
+from sklearn.model_selection import cross_validate
+from sklearn.neighbors import NearestCentroid, KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing 
+
+from utils import model_search, preprocessor, MasterModel
+
+def main():
+    
+    print("Performing preprocessing and dataframe generation")
+    # year and date can be used for classifying families because our 
+    # model performs retroactive classification
+    raw_df.drop(['Unnamed: 0'], axis=1, inplace=True)
+    raw_df_test.drop(['Unnamed: 0'], axis=1, inplace=True)
+    raw_df.sort_values(by=['year', 'day'], inplace=True)
+    raw_df.reset_index(drop=True, inplace=True)
+    
+    df = preprocessor(raw_df)
+
+    features = [c for c in df.columns if c not in ['label']]
+
+    # specify only working on classifying families given a transaction is ransomware
+    df_ransom = preprocessor(raw_df.loc[raw_df.label != 28])
+    # filter for families with more occurances than 4 to allow for cross validation
+    df_subset = df_ransom[~df_ransom.label.isin(df_ransom.label.value_counts()[df_ransom.label.value_counts()<=4].index.to_list())]
+    
+    testing_data = [{'param_dist': { 'objective': 'binary:logistic', 'verbosity':0, 'n_estimators':2},
+                'clf': xgb.XGBClassifier,
+                'name': 'xgb',
+                'ovr': True},
+                {'param_dist': {'objective':'multi:softmax', 'verbosity':0, 'num_class': 28, 'n_estimators':2},
+                'clf': xgb.XGBClassifier,
+                'name': 'xgb multi inherent',
+                'ovr': False},
+                {'param_dist': {'class_weight': 'balanced', },
+                'clf': ExtraTreesClassifier,
+                'name': 'et',
+                'ovr': False},
+                {'param_dist': {},
+                'clf': MLPClassifier,
+                'name': 'mlp',
+                'ovr': False},
+                {'param_dist': {'class_weight': 'balanced', },
+                'clf': RidgeClassifier,
+                'name': 'ridge',
+                'ovr': False},
+                {'param_dist': {},
+                'clf': KNeighborsClassifier,
+                'name': 'kneighbors',
+                'ovr': False},
+                {'param_dist': {'class_weight': 'balanced', },
+                'clf': SGDClassifier,
+                'name': 'sgd',
+                'ovr': False},
+                {'param_dist': {'class_weight': 'balanced', 'multi_class': 'ovr'},
+                'clf': LinearSVC,
+                'name': 'lsvc',
+                'ovr': False},
+               {'param_dist': {'class_weight': 'balanced', },
+                'clf': RandomForestClassifier,
+                'name': 'rf',
+                'ovr': True},
+               {'param_dist': {'class_weight': 'balanced', 'multi_class': 'ovr'},
+                'clf': LogisticRegression,
+                'name': 'lr ovr',
+                'ovr': False},
+                {'param_dist': {'class_weight': 'balanced', 'multi_class': 'multinomial'},
+                'clf': LogisticRegression,
+                'name': 'lr multiclass',
+                'ovr': False},]
+
+    print("|"*40)
+    print("Searching for best ransomware families classifier")
+    print("|"*40)
+    X = df_subset[features]
+    y = df_subset.label
+
+    X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.25,random_state=0)
+    all_clfs, df_scores = model_search(X_train, X_test, y_train,  testing_data)
+    
+    print(df_scores)
+    
+    print("\n")
+    print("Generating final multiclass ransom family classifier (chosen from the highest performance...)")
+    # reinitialize X and y to include ALL ransom families
+    X = df_subset[features]
+    y = df_subset.label
+
+    X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.40, random_state=22)
+
+    multiclassClf = RandomForestClassifier
+    multiclassClf = multiclassClf(class_weight='balanced').fit(X_train, y_train)
+    # obtain class probabilities
+    y_prob = multiclassClf.predict_proba(X_test)
+
+    # score based on weighted roc auc
+    score = roc_auc_score(y_test, y_prob, average='weighted', multi_class='ovr')
+
+    print("Random Forest roc auc wieghted score: ", score)
+    print("-"*40)
+    print("\nGenerating dictionary of known ransomware addresses...")
+    X = df[features]
+    y = df.label
+
+    # manifest the dictionary of known addresses
+    dict_known_address = {}
+
+    for i, row in df.iterrows():
+        if row.label != 28:
+            if row.address not in dict_known_address:
+                dict_known_address.update({row.address: row.label})
+    print("-"*40)
+    print("\nBuilding the binary classifier (whether or not a transaction is ransomware)")
+    binaryClf = RandomForestClassifier
+
+    # reinitialize X and y to include ALL ransom families
+    X = df[features]
+    y = (df.label == 28).astype(int)
+
+    X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.40, random_state=22)
+
+    binaryClf = RandomForestClassifier
+    binaryClf = binaryClf(class_weight='balanced').fit(X_train, y_train)
+    # obtain class probabilities
+    y_prob = binaryClf.predict_proba(X_test)[:, 1]
+
+    # score based on roc auc
+    score = roc_auc_score(y_test, y_prob)
+
+    print("Binary classifier roc auc score: ", score)
+    
+    print("-"*40)
+    print("\nTesting the MasterModel using the trained binaryClf and multiclassClf as well as the dictionary of known ransomware addresses")
+
+    X = df[features]
+    y = df.label
+
+    X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.25,random_state=0)
+
+    master = MasterModel(dict_known_address, binaryClf, multiclassClf)
+
+
+    preds = master.predictions(X_test)
+    score = accuracy_score(y_test, preds)
+    print("Master model score: ", score)
+    
+    
+
+if __name__ == '__main__':
+    
+    try:
+        raw_df_test = pd.read_csv('../DataHacks-2021/Intermediate Track 1 (Bitcoin)/Datasets/bitcoin_test.csv')
+        raw_df = pd.read_csv('../DataHacks-2021/Intermediate Track 1 (Bitcoin)/Datasets/bitcoin_train.csv')
+    except:
+        print("DATASETS AREN'T IN ../DataHacks-2021/Intermediate Track 1 (Bitcoin)/Datasets/bitcoin_*.csv'")
+
+    main()
---
+++ utils.py
@@ -0,0 +1,145 @@
+import warnings
+warnings.filterwarnings('ignore')
+
+import pandas as pd
+import numpy as np
+import xgboost as xgb
+from sklearn.model_selection import train_test_split
+from sklearn.multiclass import OneVsRestClassifier, OneVsOneClassifier
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.linear_model import LogisticRegression, RidgeClassifier, SGDClassifier
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
+from sklearn.svm import LinearSVC
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import confusion_matrix, plot_confusion_matrix, accuracy_score, roc_auc_score
+from sklearn.model_selection import cross_validate
+from sklearn.neighbors import NearestCentroid, KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing 
+
+
+def genClf(clf, param_dist, X_train, X_test, y_train, name, ovr=False):
+    
+    print("-"*50)
+    print(f"Generating '{name}' MultiClass classifier...")
+    
+    # if we want to, wrap the classifier in the ovr scikit-learn wrapper
+        # it builds a classifier for each label
+    if ovr:
+        clf = OneVsRestClassifier(clf(**param_dist)).fit(X_train, y_train)
+    else:
+        clf = clf(**param_dist).fit(X_train, y_train)        
+
+    scoring = {'acc': 'accuracy', 'roc_auc_ovr': 'roc_auc_ovr', 'roc_auc_ovr_weights': 'roc_auc_ovr_weighted'}
+    cv_data = cross_validate(clf, X_train, y_train, scoring=scoring, cv=3, n_jobs=-1)
+    keys = ['acc', 'roc_auc_ovr', 'roc_auc_ovr_weights']
+    
+    scores = [np.mean(cv_data["test_"+key]) for key in keys]
+    ret_scores = {name : scores}
+    
+    return clf, ret_scores
+
+
+
+def model_search(X_train, X_test, y_train, testing_data):
+    """ Tests an assortment of models to heuristically determine the best choice
+    
+    Returns
+    -------
+    (list(object), list(list(float)))
+        a tuple of all the trained classifier objects and a dataframe of scores
+    """
+    # initialize return objects
+    all_scores = {}
+    all_clfs = []
+    
+    # iterate through testing data
+    for i in testing_data:
+        # generate the clf and score
+        clf, ret_scores = genClf(i['clf'], i['param_dist'], X_train, X_test, y_train, name=i['name'], ovr=i['ovr'])
+        # append the return values
+        all_clfs.append(clf)
+        all_scores.update(ret_scores)
+    
+    # craft dataframe of all scores
+    df_scores = pd.DataFrame.from_dict(all_scores, 
+                                       orient="index", 
+                                       columns=["raw_acc", "macro_roc_auc_ovr", "weighted_roc_auc_ovr"])
+
+    return all_clfs, df_scores
+
+
+# convert data to standard form (scaled by standard deviation around the mean)
+def standardize(feature):
+    mu = feature.mean() # compute mean
+    stdev = feature.std() # compute standard deviation
+    return (feature - mu) / stdev
+
+def preprocessor(df):
+    # encode addresses and labels for ease of access
+    le = preprocessing.LabelEncoder()
+    df['address'] = le.fit_transform(df.address)
+    df['label'] = le.fit_transform(df.label)
+    # sort values by date and reset index
+    df['date'] = df.year + np.round(df.day / 365,3)
+    df = df.sort_values(by='date').reset_index(drop=True)
+    # add address counts, number of times given address appears in the data
+    df['address_count'] = df.groupby('address')['address'].transform('size')
+    # add cumulative count column, cumulative number of times address appears in the data
+    df['address_cumcount'] = df.groupby('address').cumcount() # add cumuative address counts to dataframe
+    # add label cumcount
+    df['label_cumcount'] = df.groupby('label').cumcount() # add cumulative label counts to dataframe
+    
+    #standardize the data
+    to_standardize = ['length','weight','count','looped','neighbors','income']
+    standardized_features = df[to_standardize].apply(standardize)
+    df = df.drop(to_standardize,axis=1).join(df[to_standardize].apply(standardize))
+    return df
+
+
+
+class MasterModel:
+    """ Master model object for performing three steps in ransomware classification
+    
+    Parameters
+    ----------
+    dictKnownAddress : dict()
+        the dictionary of known ransomware adresses
+    binaryClf : object
+        the binary classifier for whether or not a transaction is ransomware. Needs to be already fitted!
+    multiclassClf : object
+        the multiclass classifier for, if it is a ransomware transaction, which family is it in. Needs to be already fitted!
+        
+    """
+    def __init__(self, dictKnownAddress, binaryClf, multiclassClf, is_fit=False):
+        self.dictKnownAddress = dictKnownAddress
+        self.binaryClf = binaryClf
+        self.multiclassClf = multiclassClf
+    
+    
+    def predictions(self, X):
+        ret = []
+        for i, row in X.iterrows():
+            ret.append(self.predict(row))
+            
+        return ret
+            
+    
+    def predict(self, X):
+        # if the address is in the dict of known addresses, return the label
+        if X[0] in self.dictKnownAddress:
+            return self.dictKnownAddress[X[0]]
+        
+        binary_predict = self.binaryClf.predict([X])
+        
+        # if it is ransomware
+        if binary_predict == 0:
+            # then return the mutliclass prediciton
+            label = self.multiclassClf.predict([X])
+            return label[0]
+        else:
+            return 28
+        
+        
+        
+    
